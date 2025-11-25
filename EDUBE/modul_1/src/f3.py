c925bc8a6b7f64b168b28b56d123bac08556c1e6
Jakob Derzapf <jakob.derzapf@gfn.de>
2025-11-25 11:56:47 +0100

    neustes Update

---
+++ EDUBE/modul_1/src/f3.py
@@ -0,0 +1,10 @@
+"""
+Das Ergebnis sollte derselbe sein wie zuvor, 
+da wir tatsächlich dieselben Entitäten wie zuvor verwendet haben: 
+"""
+
+
+from math import sin, pi
+
+print(sin(pi/2))
+
---
+++ EDUBE/modul_1/src/f5.py
@@ -0,0 +1,14 @@
+"""
+Importieren eines Moduls: *
+In der dritten Methode ist die Syntax von 's eine aggressivere Form der zuvor präsentierten:import
+
+from module import *
+"""
+
+"""
+Importieren eines Moduls: das as-Schlüsselwort
+modul als alias importieren
+"""
+import math as kunstname
+
+print(kunstname.sin(kunstname.pi/2))

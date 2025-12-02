60b8e1cc87f65a9910c78b59d97ebbfefece7677
Jakob Derzapf <jakob.derzapf@gfn.de>
2025-12-02 16:20:47 +0100

    neustes Update

--- EDUBE/modul_1/src/f21_module.py
+++ EDUBE/modul_1/src/f21_module.py
@@ -20,9 +20,14 @@ Wenn du eine Datei direkt ausführst, wird ihre Variable auf gesetzt __name____m
 Wenn eine Datei als Modul importiert wird, wird ihre Variable auf den Dateinamen gesetzt (ausgenommen .py__name__)
 """
 
+counter = 0
 # bsp.
 if __name__ == "__main__":
     print("Ich werde 'innerhalb der Datei' als modul aufgerufen")
 else:
     print("Ich werde 'außerhalb der datei' als modul aufgerufen")
 
+"""
+Dies geschieht, indem man dem Namen der Variablen mit (einem Unterstrich) oder (zwei Unterstrichen) (zwei Unterstrichen) voranstellt, 
+aber denken Sie daran, dass es nur eine Konvention ist. Die Nutzer deines Moduls könnten es befolgen oder auch nicht.___
+"""
--- training.py
+++ training.py
@@ -1,1 +1,11 @@
-print("test")
+liste = []  # leere Liste
+
+while True:
+    eingabe = input("Gib einen Wert ein (oder 'stop' zum Beenden): ")
+
+    if eingabe == "stop":
+        break
+
+    liste.append(eingabe)  # Wert an Liste anhängen
+
+print("Die gespeicherten Werte in der liste sind: ", liste, len(liste))

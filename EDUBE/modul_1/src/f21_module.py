2e0faa169ef68c9051c65d95f78132a728509b4d
Jakob Derzapf <jakob.derzapf@gfn.de>
2025-12-02 16:33:55 +0100

    neustes Update

--- EDUBE/modul_1/src/f21_module.py
+++ EDUBE/modul_1/src/f21_module.py
@@ -31,3 +31,35 @@ else:
 Dies geschieht, indem man dem Namen der Variablen mit (einem Unterstrich) oder (zwei Unterstrichen) (zwei Unterstrichen) voranstellt, 
 aber denken Sie daran, dass es nur eine Konvention ist. Die Nutzer deines Moduls könnten es befolgen oder auch nicht.___
 """
+
+#!/usr/bin/env python3 
+
+""" module.py - an example of a Python module """
+
+__counter = 0
+
+
+def suml(the_list):
+    global __counter
+    __counter += 1
+    the_sum = 0
+    for element in the_list:
+        the_sum += element
+    return the_sum
+
+
+def prodl(the_list):
+    global __counter    
+    __counter += 1
+    prod = 1
+    for element in the_list:
+        prod *= element
+    return prod
+
+
+if __name__ == "__main__":
+    print("I prefer to be a module, but I can do some tests for you.")
+    my_list = [i+1 for i in range(5)]
+    print(suml(my_list) == 15)
+    print(prodl(my_list) == 120)
+
---
+++ EDUBE/modul_1/src/f23.py
@@ -0,0 +1,26 @@
+"""
+wie Python nach Modulen sucht. 
+Es gibt eine spezielle Variable (eigentlich eine Liste), die alle Orte (Ordner/Verzeichnisse) speichert, die durchsucht werden, 
+um ein Modul zu finden, das von der Importinstruktion angefordert wurde.
+
+Python durchsucht diese Ordner in der Reihenfolge, in der sie in der Liste aufgeführt sind 
+– wenn das Modul in keinem dieser Verzeichnisse gefunden werden kann, schlägt der Import fehl.
+
+Andernfalls wird der erste Ordner mit einem Modul mit dem gewünschten Namen berücksichtigt 
+(wenn einer der verbleibenden Ordner ein Modul mit diesem Namen enthält, wird er ignoriert).
+
+Die Variable heißt , und sie ist über das Modul mit dem Namen zugänglich. So können Sie seinen regulären Wert überprüfen: pathsys
+
+C:\Users\user
+C:\Users\user\AppData\Local\Programs\Python\Python36-32\python36.zip
+C:\Users\user\AppData\Local\Programs\Python\Python36-32\DLLs
+C:\Users\user\AppData\Local\Programs\Python\Python36-32\lib
+C:\Users\user\AppData\Local\Programs\Python\Python36-32
+C:\Users\user\AppData\Local\Programs\Python\Python36-32\lib\site-packages
+"""
+
+import sys
+
+for p in sys.path:
+    print(p)
+

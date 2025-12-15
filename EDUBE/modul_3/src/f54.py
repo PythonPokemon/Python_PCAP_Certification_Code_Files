e897448b923e98c8db148e6230bb418778a2463f
Jakob Derzapf <jakob.derzapf@gfn.de>
2025-12-15 15:17:07 +0100

    neustes Update

--- EDUBE/modul_3/src/f54.py
+++ EDUBE/modul_3/src/f54.py
@@ -1,13 +1,50 @@
 """
+Eigene Ausnahmen – vollständiges Beispiel mit Vererbung
 
+In diesem Beispiel werden zwei eigene Ausnahmen verwendet:
+- PizzaFehler        → allgemeiner Fehler bei der Pizza-Bestellung
+- ZuVielKaeseFehler  → spezieller Fehler (Unterklasse von PizzaFehler)
+
+Wichtige Lernziele (PCAP ):
+- Eigene Ausnahmen von Exception ableiten
+- Ausnahme-Vererbung korrekt nutzen
+- Reihenfolge der except-Blöcke ist entscheidend
+- Ausnahmeobjekte können Zusatzdaten tragen (Kontext!)
 """
 
+class PizzaFehler(Exception):
+    def __init__(self, pizza, meldung):
+        super().__init__(meldung)              # Exception korrekt initialisieren
+        self.pizza = pizza                     # betroffene Pizza speichern
 
 
+class ZuVielKaeseFehler(PizzaFehler):
+    def __init__(self, pizza, kaese, meldung):
+        super().__init__(pizza, meldung)       # PizzaFehler initialisieren
+        self.kaese = kaese                     # zusätzliche Detailinformation
 
 
+def pizza_machen(pizza, kaese):
+    if pizza not in ['margherita', 'capricciosa', 'calzone']:
+        raise PizzaFehler(pizza, "Pizza nicht auf der Karte")
+    if kaese > 100:
+        raise ZuVielKaeseFehler(pizza, kaese, "Zu viel Käse")
+    print("Pizza fertig!")
 
-"""
 
-"""
+# ---------------------------------------------------------
+# Testläufe
+# ---------------------------------------------------------
+bestellungen = [
+    ('calzone', 0),
+    ('margherita', 110),
+    ('mafia', 20)
+]
 
+for pz, ks in bestellungen:
+    try:
+        pizza_machen(pz, ks)
+    except ZuVielKaeseFehler as fehler:
+        print(fehler, ":", fehler.kaese)       # spezieller Fehler zuerst!
+    except PizzaFehler as fehler:
+        print(fehler, ":", fehler.pizza)
--- EDUBE/modul_3/src/f55.py
+++ EDUBE/modul_3/src/f55.py
@@ -1,13 +1,82 @@
 """
+Eigene Ausnahmen erstellen – Fortsetzung
+Custom-Exception-Hierarchie (PizzaError / TooMuchCheeseError)
 
+-----------------------------------------------------------
+Problem: Erweiterte Fehlerbehandlung in eigenen Modulen
+-----------------------------------------------------------
+Wir möchten eigene Ausnahmen definieren, um Fehler in einem
+domänenspezifischen Kontext (z. B. Pizzeria-Simulation)
+klarer zu unterscheiden.
+
+Beispiel:
+• Ungültige Pizza → PizzaError
+• Zu viel Käse     → TooMuchCheeseError
+
+Dadurch können Fehler gezielt abgefangen werden.
+
+-----------------------------------------------------------
+Schwäche der ursprünglichen Lösung
+-----------------------------------------------------------
+Wenn der Konstruktor der eigenen Ausnahme Pflichtargumente
+verlangt (pizza, message, …), kann raise PizzaError ohne
+Argumente NICHT funktionieren.
+
+→ Lösung: Defaultwerte für alle Parameter setzen.
+
+So bleiben beide Varianten gültig:
+• raise PizzaError
+• raise PizzaError("margherita", "Fehlerbeschreibung")
+
+-----------------------------------------------------------
+Was muss man bei try/except beachten?
+-----------------------------------------------------------
+Wenn except TooMuchCheeseError entfernt wird:
+→ Jede dieser Ausnahmen wird als PizzaError behandelt  
+  (weil TooMuchCheeseError eine Unterklasse IST).
+
+Wenn except PizzaError entfernt wird:
+→ TooMuchCheeseError wird korrekt abgefangen  
+→ PizzaError NICHT → Programm bricht ab
+
+-----------------------------------------------------------
+⚠️ WICHTIG FÜR EDUBE / PCAP
+-----------------------------------------------------------
+• Eigene Exceptions IMMER von Exception ableiten  
+• Reihenfolge der except-Zweige ist entscheidend:
+  Unterklassen müssen VOR Oberklassen stehen
+• Konstruktorparameter brauchen Defaults, sonst:
+  raise MyError  führt zu TypeError
+
+-----------------------------------------------------------
+Typische Prüfungsfallen
+-----------------------------------------------------------
+• Ausnahme wird nicht gefangen, weil except-Reihenfolge falsch ist
+• Ausnahme kann nicht erzeugt werden, weil Konstruktor-Args fehlen
+• Unterklasse überschattet Oberklasse, wenn except falsch sortiert ist
 """
 
+class PizzaFehler(Exception):
+    def __init__(self, pizza='unbekannt', nachricht=''):
+        Exception.__init__(self, nachricht)
+        self.pizza = pizza                              # betroffene Pizza
 
+class ZuVielKaeseFehler(PizzaFehler):
+    def __init__(self, pizza='unbekannt', kaese='>100', nachricht=''):
+        PizzaFehler.__init__(self, pizza, nachricht)    # Nachricht an Superklasse
+        self.kaese = kaese                              # zu viel verwendeter Käse
 
+def pizza_herstellen(pizza, kaese):
+    if pizza not in ['margherita', 'capricciosa', 'calzone']:
+        raise PizzaFehler                               # ungültige Pizza
+    if kaese > 100:
+        raise ZuVielKaeseFehler                         # zu viel Käse
+    print("Pizza fertig!")
 
-
-
-"""
-
-"""
-
+for (pz, ch) in [('calzone', 0), ('margherita', 110), ('mafia', 20)]:
+    try:
+        pizza_herstellen(pz, ch)
+    except ZuVielKaeseFehler as zkf:
+        print(zkf, ':', zkf.kaese)
+    except PizzaFehler as pf:
+        print(pf, ':', pf.pizza)
